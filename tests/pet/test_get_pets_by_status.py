5043250a1cec9d78432112a917239a2c5ab45773
imateenkovv <glaymonow@gmail.com>
2025-07-15 14:04:30 +0300

    add from paycharm

--- tests/pet/test_get_pets_by_status.py
+++ tests/pet/test_get_pets_by_status.py
@@ -1,24 +1,19 @@
+
 import requests
+import pytest
 
 BASE_URL = "https://petstore.swagger.io/v2"
+GOAL_URL = f"{BASE_URL}/pet/findByStatus"
 
+# Используем параметризацию для статусов
+@pytest.mark.parametrize("status", ["available", "pending", "sold"])
+def test_get_pets_by_status(status):
 
-def test_get_pets_by_status_avialable():
     response = requests.get(
-        f"{BASE_URL}/pet/findByStatus", params={"status": "available"}
+        GOAL_URL, params={"status": status}
     )
-    assert response.status_code == 200
-
-
-def test_get_pets_by_status_sold():
-    response = requests.get(
-        f"{BASE_URL}/pet/findByStatus", params={"status":"sold"}
-    )
-    assert response.status_code == 200
-
-
-def test_get_pets_by_status_pending():
-    response = requests.get(
-        f"{BASE_URL}/pet/findByStatus", params={"status":"pending"}
-    )
-    assert response.status_code == 200
+    assert response.status_code == 200, f"Status code is not 200 for status '{status}'"
+    response_json = response.json()
+    assert isinstance(response_json, list), "Response is not a list"
+    for pet in response_json:
+        assert pet['status'] == status, f"Pet with id {pet.get('id')} has wrong status"
--- tests/pet/test_post_add_pet.py
+++ tests/pet/test_post_add_pet.py
@@ -1,12 +1,17 @@
 import requests
 
 BASE_URL = "https://petstore.swagger.io/v2"
-
+GOAL_URL = f"{BASE_URL}/pet"
 
 def test_add_new_pet():
     pet_data_for_adding = {"name": "test_name", "photoUrls": ["https://testurls/test"]}
 
-    response = requests.post(f"{BASE_URL}/pet", json=pet_data_for_adding)
+    response = requests.post(GOAL_URL, json=pet_data_for_adding)
     assert response.status_code == 200
     response_json = response.json()
     assert response_json["name"] == pet_data_for_adding["name"]
+
+def test_add_new_pet_with_empty_body():
+    pet_data_for_adding = {}
+    response = requests.post(GOAL_URL, json=pet_data_for_adding)
+    assert response.status_code == 400
