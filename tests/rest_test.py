dc1880a93e9a8f5be54a022fd6211b6a03ce26f9
imateenkovv <glaymonow@gmail.com>
2025-07-15 11:22:17 +0300

    add new tests

--- tests/rest_test.py
+++ tests/rest_test.py
@@ -8,3 +8,17 @@ def test_get_pets_by_status_avialable():
         f"{BASE_URL}/pet/findByStatus", params={"status": "available"}
     )
     assert response.status_code == 200
+
+
+def test_get_pets_by_status_sold():
+    response = requests.get(
+        f"{BASE_URL}/pet/findByStatus", params={"status":"sold"}
+    )
+    assert response.status_code == 200
+    
+    
+def test_get_pets_by_status_pending():
+    response = requests.get(
+        f"{BASE_URL}/pet/findByStatus", params={"status":"pending"}
+    )
+    assert response.status_code == 200
